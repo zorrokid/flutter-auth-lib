4e5420cd813ef56fc1c28d74b66636e4d5bd7bf5
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-09 20:12:59 +0200

    More exports.

--- lib/auth_lib.dart
+++ lib/auth_lib.dart
@@ -1,3 +1,5 @@
 library auth_lib;
 
 export 'src/widgets/user_form.dart' show UserForm;
+export 'src/controllers/user_form_controller.dart' show UserFormController;
+export 'src/services/auth_service.dart' show AuthFailure, AuthenticationService;
